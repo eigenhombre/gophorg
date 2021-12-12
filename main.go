ead55e067f9a02476312b4eb71f6b477b05c3595
John Jacobsen <eigenhombre@gmail.com>
2021-12-11 21:42:19 -0600

    Make sure we have blogpath. Fixes #10

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 	var blogpath string
-	flag.StringVar(&blogpath, "b", "blogpath", "Specify path to Org export / blog files.")
+	flag.StringVar(&blogpath, "b", "", "Specify path to Org export / blog files.")
 	flag.Parse()
+	if blogpath == "" {
+		flag.PrintDefaults()
+		os.Exit(-1)
+	}
 	fmt.Println(blogpath)
 }
