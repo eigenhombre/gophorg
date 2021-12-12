0dbc9e8a67ddde5498823ae08577f9cb59e8de57
John Jacobsen <eigenhombre@gmail.com>
2021-12-11 21:58:48 -0600

    List HTML files in `blogpath`. Fixes #9.

--- main.go
+++ main.go
@@ -3,8 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 )
+
+func listHtmlFilesInDir(blogpath string) {
+	files, err := ioutil.ReadDir(blogpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".html") {
+			fmt.Println(strings.Replace(file.Name(),
+				".html", "", 1))
+		}
+	}
+}
 
 func main() {
 	var blogpath string
@@ -14,5 +30,5 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
-	fmt.Println(blogpath)
+	listHtmlFilesInDir(blogpath)
 }
