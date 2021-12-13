e75ba40c47896fc8c2e15cf6c575d38cc22c4315
John Jacobsen <eigenhombre@gmail.com>
2021-12-13 09:21:24 -0600

    Copy HTML files from input to output. Fixes #12.

--- main.go
+++ main.go
@@ -9,32 +9,44 @@ import (
 	"strings"
 )
 
-// FIXME: Decomplect this
-func readHtmlFilesInDir(blogpath string) {
+func findHtmlFilesInDir(blogpath string) []string {
+	var ret []string
 	files, err := ioutil.ReadDir(blogpath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".html") {
-			fmt.Print(strings.Replace(file.Name(),
-				".html", "", 1) + " ")
-			content, err := ioutil.ReadFile(blogpath + "/" + file.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(len(content))
+			ret = append(ret, file.Name())
+		}
+	}
+	return ret
+}
+
+func copyBlogHtmlFiles(blogpath string, outpath string) {
+	// Ignoring errors for `mkdir -p` equivalent.  How do do better?
+	os.Mkdir(outpath, 0755)
+	for _, file := range findHtmlFilesInDir(blogpath) {
+		content, err := ioutil.ReadFile(blogpath + "/" + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = ioutil.WriteFile(outpath+"/"+file, []byte(content), 0644)
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
 }
 
 func main() {
-	var blogpath string
-	flag.StringVar(&blogpath, "b", "", "Specify path to Org export / blog files.")
+	var blogpath, outpath string
+	flag.StringVar(&blogpath, "b", "", "Specify path to Org export / blog files")
+	flag.StringVar(&outpath, "o", "/tmp/blog", "Specify path for target directory")
 	flag.Parse()
 	if blogpath == "" {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
-	readHtmlFilesInDir(blogpath)
+	copyBlogHtmlFiles(blogpath, outpath)
+	fmt.Println("OK")
 }
