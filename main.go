6aeb792b416552db2b145e418d640d1bd52e2662
John Jacobsen <eigenhombre@gmail.com>
2021-12-13 21:07:35 -0600

    Refactoring (fixes #13).

--- main.go
+++ main.go
@@ -9,33 +9,48 @@ import (
 	"strings"
 )
 
-func findHtmlFilesInDir(blogpath string) []string {
+func findHtmlFilesInDir(blogpath string) ([]string, error) {
 	var ret []string
 	files, err := ioutil.ReadDir(blogpath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".html") {
 			ret = append(ret, file.Name())
 		}
 	}
-	return ret
+	return ret, nil
 }
 
-func copyBlogHtmlFiles(blogpath string, outpath string) {
-	// Ignoring errors for `mkdir -p` equivalent.  How do do better?
-	os.Mkdir(outpath, 0755)
-	for _, file := range findHtmlFilesInDir(blogpath) {
-		content, err := ioutil.ReadFile(blogpath + "/" + file)
+func processHtmlFile(blogpath, outpath, file string) error {
+	content, err := ioutil.ReadFile(blogpath + "/" + file)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(outpath+"/"+file, []byte(content), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func copyBlogHtmlFiles(blogpath string, outpath string) (int, error) {
+	var err error
+	if err = os.MkdirAll(outpath, 0755); err != nil {
+		return 0, err
+	}
+	files, err := findHtmlFilesInDir(blogpath)
+	if err != nil {
+		return 0, err
+	}
+	for _, file := range files {
+		err := processHtmlFile(blogpath, outpath, file)
 		if err != nil {
-			log.Fatal(err)
-		}
-		err = ioutil.WriteFile(outpath+"/"+file, []byte(content), 0644)
-		if err != nil {
-			log.Fatal(err)
+			return 0, err
 		}
 	}
+	return len(files), nil
 }
 
 func main() {
@@ -47,6 +62,9 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(-1)
 	}
-	copyBlogHtmlFiles(blogpath, outpath)
-	fmt.Println("OK")
+	numfiles, err := copyBlogHtmlFiles(blogpath, outpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Processed %d exported files.\n", numfiles)
 }
